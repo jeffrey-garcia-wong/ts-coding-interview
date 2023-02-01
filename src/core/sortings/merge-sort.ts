e826590f244b78ac19b658d7f79e6f01d3eaf500
jeffrey garcia <jeffrey-garcia@outlook.com>
2023-02-01 16:33:24 +0000

    improve quick sort and merge sort implementation

--- src/core/sortings/merge-sort.ts
+++ src/core/sortings/merge-sort.ts
@@ -1,52 +1,50 @@
 class MergeSort {
     public execute(input:Array<number>):Array<number> {
-        return this.solutionV1(input);
-    }
-
-    private solutionV1(input:Array<number>):Array<number> {
-        this.mergeSort(input, 0, input.length - 1, new Array<number>(input.length).fill(0));
+        this.solutionV1(input, 0, input.length - 1, new Array<number>(input.length).fill(0));
         return input;
     }
 
-    private mergeSort(input:Array<number>, start:number, end:number, tmp:Array<number>):void {
+    private solutionV1(input:Array<number>, start:number, end:number, tmp:Array<number>):void {
         if ((end - start) < 1) return;
-        const splitIndex = start + Math.floor((end - start) / 2);
-        this.mergeSort(input, start, splitIndex, tmp);
-        this.mergeSort(input, splitIndex+1, end, tmp);
-        this.merge(input, start, splitIndex, splitIndex+1, end, tmp);
-    }
 
-    private merge(input:Array<number>, leftStart:number, leftEnd:number, rightStart:number, rightEnd:number, tmp:Array<number>):void {
-        const start:number = leftStart;
-        const end:number = rightEnd;
+        const splitIndex = start + Math.floor((end - start) / 2);
+        this.solutionV1(input, start, splitIndex, tmp);
+        this.solutionV1(input, splitIndex+1, end, tmp);
         
-        let current = leftStart;
-        while (leftStart <= leftEnd && rightStart <= rightEnd) {
-            if (input[leftStart] < input[rightStart]) {
+        const merge = ((input:Array<number>, leftStart:number, leftEnd:number, rightStart:number, rightEnd:number, tmp:Array<number>):void => {
+            const start:number = leftStart;
+            const end:number = rightEnd;
+            
+            let current = leftStart;
+            while (leftStart <= leftEnd && rightStart <= rightEnd) {
+                if (input[leftStart] < input[rightStart]) {
+                    tmp[current] = input[leftStart];
+                    leftStart += 1;
+                } else {
+                    tmp[current] = input[rightStart];
+                    rightStart += 1;
+                }
+                current += 1;
+            }
+    
+            while (leftStart <= leftEnd) {
                 tmp[current] = input[leftStart];
                 leftStart += 1;
-            } else {
+                current += 1;
+            }
+    
+            while (rightStart <= rightEnd) {
                 tmp[current] = input[rightStart];
                 rightStart += 1;
+                current += 1;
             }
-            current += 1;
-        }
+    
+            for (let i=start; i<=end; i++) {
+                input[i] = tmp[i];
+            }
+        });
 
-        while (leftStart <= leftEnd) {
-            tmp[current] = input[leftStart];
-            leftStart += 1;
-            current += 1;
-        }
-
-        while (rightStart <= rightEnd) {
-            tmp[current] = input[rightStart];
-            rightStart += 1;
-            current += 1;
-        }
-
-        for (let i=start; i<=end; i++) {
-            input[i] = tmp[i];
-        }
+        merge(input, start, splitIndex, splitIndex+1, end, tmp);
     }
 }
 
--- src/core/sortings/quick-sort.ts
+++ src/core/sortings/quick-sort.ts
@@ -1,33 +1,47 @@
 class QuickSort {
 
     public execute(input:Array<number>):Array<number> {
-        return this.solutionV1(input);
+        this.solutionV1(input, 0, input.length-1);
+        return input;
     }
 
-    private solutionV1(input:Array<number>):Array<number> {
-        return this.quicksort(input);
-    }
+    private solutionV1(input:Array<number>, start:number, end:number):void {
+        if ((end - start) < 1) return;
 
-    private quicksort(input:Array<number>):Array<number> {
-        if (input.length < 2) return input;
+        const partition = ((input:Array<number>, start:number, end:number):number => {
+            const middle = start + Math.floor((end - start)/2);
+            const pivot = input[middle];
 
-        let lower = new Array<number>();
-        let higher = new Array<number>();
+            const swap = ((i:number, j:number) => {
+                const backup = input[i];
+                input[i] = input[j];
+                input[j] = backup;
+            })
 
-        let pivot = input[0];
-        for (let i=1; i<input.length; i++) {
-            if (input[i] < pivot) {
-                lower.push(input[i]);
-            } else {
-                higher.push(input[i]);
+            swap(middle, end);
+
+            let left = start;
+            let right = end - 1;
+
+            while (left <= right) {
+                while (input[left] < pivot) left += 1;
+                while (input[right] > pivot) right -= 1;
+
+                if (left <= right) {
+                    swap(left, right);
+                    left += 1;
+                    right -= 1;
+                }
             }
-        }
 
-        const output = new Array<number>();
-        output.push(... this.quicksort(lower));
-        output.push(pivot);
-        output.push(... this.quicksort(higher));
-        return output;
+            swap(left, end);
+
+            return left;
+        });
+
+        let splitIndex = partition(input, start, end);
+        this.solutionV1(input, start, splitIndex - 1);
+        this.solutionV1(input, splitIndex + 1, end);
     }
 }
 
